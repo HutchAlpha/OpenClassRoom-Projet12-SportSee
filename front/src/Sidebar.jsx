8eca106664cdcdb5a834ad7daffd8292dfa19716
HutchAlpha <qboisset@hotmail.com>
2026-03-08 19:16:46 +0100

    Ajout Sidebar (j'ai créer dossier Assets pour les IMG )

---
+++ front/src/Sidebar.jsx
@@ -0,0 +1,30 @@
+
+import Yoga from "../public/assets/yoga.svg";
+import Swimming from "../public/assets/swimming.svg";
+import Cycling from "../public/assets/cycling.svg";
+import Bodybuilding from "../public/assets/bodybuilding.svg";
+import './styles/Sidebar.scss' 
+
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebar__item">
+                <img src={Yoga} alt="Yoga" />
+            </div>
+            <div className="sidebar__item">
+                <img src={Swimming} alt="Natation" />
+            </div>
+            <div className="sidebar__item">
+                <img src={Cycling} alt="Cyclisme" />
+            </div>
+            <div className="sidebar__item">
+                <img src={Bodybuilding} alt="Musculation" />
+            </div>
+
+            <p>Copyright, SportSee 2026</p>
+        </div>
+    );
+}
+
+export default Sidebar;
--- front/src/main.jsx
+++ front/src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './styles/index.scss'
 import App from './App.jsx'
 import Header from './Header.jsx'
+import Sidebar from './Sidebar.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
+    <Sidebar />
     <App />
   </StrictMode>,
 )
