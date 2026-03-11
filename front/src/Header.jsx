2634ae9a0aa0162e6ef2fcd715d7b5600f469a51
HutchAlpha <qboisset@hotmail.com>
2026-03-11 10:48:00 +0100

    Amélioration Header + Sidebar

--- front/src/Header.jsx
+++ front/src/Header.jsx
@@ -2,22 +2,21 @@ import Logo from '../public/logo.svg'
 import './styles/Header.scss'
 
 function Header() {
-    return (
-        <header className="header">
-            <div className="header__logo">
-                <img src={Logo} alt="logo SportSee" />
-            </div>
-
-            <nav className="header__nav">
-                <ul className="header__list">
-                <li className="header__item">Accueil</li>
-                <li className="header__item">Profil</li>
-                <li className="header__item">Réglage</li>
-                <li className="header__item">Communauté</li>
-                </ul>
-            </nav>
-        </header>
-    );
+	return (
+		<header className="header">
+			<div className="header__logo">
+				<img src={Logo} alt="SportSee" />
+			</div>
+			<nav className="header__nav">
+				<ul className="header__list">
+					<li className="header__item">Accueil</li>
+					<li className="header__item">Profil</li>
+					<li className="header__item">Réglage</li>	
+					<li className="header__item">Communauté</li>
+				</ul>
+			</nav>
+		</header>
+	);
 }
 
 export default Header;
--- front/src/Sidebar.jsx
+++ front/src/Sidebar.jsx
@@ -1,33 +1,31 @@
-
 import Yoga from "../public/assets/yoga.svg";
 import Swimming from "../public/assets/swimming.svg";
 import Cycling from "../public/assets/cycling.svg";
 import Bodybuilding from "../public/assets/bodybuilding.svg";
-import './styles/Sidebar.scss' 
-
+import './styles/Sidebar.scss'
 
 function Sidebar() {
-    return (
-        <div className="sidebar">
-            <div className="sidebar__item">
-                <img src={Yoga} alt="Yoga" />
-            </div>
-            <div className="sidebar__item">
-                <img src={Swimming} alt="Natation" />
-            </div>
-            <div className="sidebar__item">
-                <img src={Cycling} alt="Cyclisme" />
-            </div>
-            <div className="sidebar__item">
-                <img src={Bodybuilding} alt="Musculation" />
-            </div>
-     
-            <div className="sidebar__copyright">
-                <p>Copyright, SportSee 2026</p>
-            </div>
-
-        </div>
-    );
+	return (
+		<aside className="sidebar">
+			<div className="sidebar__icons">
+				<button className="sidebar__icon-btn">
+					<img src={Yoga} alt="Yoga" />
+				</button>
+				<button className="sidebar__icon-btn">
+					<img src={Swimming} alt="Natation" />
+				</button>
+				<button className="sidebar__icon-btn">
+					<img src={Cycling} alt="Vélo" />
+				</button>
+				<button className="sidebar__icon-btn">
+					<img src={Bodybuilding} alt="Musculation" />
+				</button>
+			</div>
+			<div className="sidebar__copyright">
+				Copyright, SportSee 2026
+			</div>
+		</aside>
+	);
 }
 
 export default Sidebar;
