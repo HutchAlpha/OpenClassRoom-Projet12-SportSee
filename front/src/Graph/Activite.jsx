43a8a81dd0a22528c3e6b47f74ffca45066648d3
HutchAlpha <qboisset@hotmail.com>
2026-03-12 19:33:07 +0100

    Ajout legendes

--- front/src/Graph/Activite.jsx
+++ front/src/Graph/Activite.jsx
@@ -1,4 +1,4 @@
-import { select, scaleBand, scaleLinear, max } from "d3";
+import { select, scaleBand, scaleLinear, max, axisBottom, axisLeft, axisRight } from "d3";
 import { useRef, useEffect } from "react";
 
 function Activite({ data }) {
@@ -10,35 +10,99 @@ function Activite({ data }) {
     const width = 825;
     const height = 320;
 
+    //! sélection du svg
     const svg = select(svgRef.current);
 
-    // nettoie le svg si rerender
+    //! nettoyage du svg
     svg.selectAll("*").remove(); 
 
+    //! configuration du svg
     svg
       .attr("width", width)
       .attr("height", height)
       .attr("viewBox", `0 0 ${width} ${height}`);
 
+    //! récupération des données
     const sessions = data.activity.sessions;
 
-    // échelle X (jours)
+    //! échelle X
     const x = scaleBand()
       .domain(sessions.map(d => d.day))
       .range([50, width - 40])
       .padding(0.4);
 
-    // échelle Y calories
+    //! échelle Y calories
     const yCalories = scaleLinear()
       .domain([0, max(sessions, d => d.calories)])
       .range([height - 40, 40]);
 
-    // échelle Y poids
+    //! échelle Y poids
     const yPoids = scaleLinear()
       .domain([minPoids(sessions) - 1, max(sessions, d => d.kilogram) + 1])
       .range([height - 40, 40]);
 
-    // barres poids (noir)
+    //! titre du graphique
+  svg
+    .append("text")
+    .attr("x", 50)
+    .attr("y", 25)
+    .attr("font-size", "15px")
+    .attr("font-weight", "600")
+    .attr("fill", "#20253A")
+    .text("Activité quotidienne");
+
+    //! Legende Poids
+    svg
+    .append("circle")
+    .attr("cx", width - 220)
+    .attr("cy", 25)
+    .attr("r", 4)
+    .attr("fill", "#282D30");
+
+    svg
+    .append("text")
+    .attr("x", width - 205)
+    .attr("y", 29)
+    .attr("font-size", "14px")
+    .attr("fill", "#74798C")
+    .text("Poids (kg)");
+
+    //! Legende Calories
+    svg
+    .append("circle")
+    .attr("cx", width - 120)
+    .attr("cy", 25)
+    .attr("r", 4)
+    .attr("fill", "#E60000");
+
+    svg
+    .append("text")
+    .attr("x", width - 105)
+    .attr("y", 29)
+    .attr("font-size", "14px")
+    .attr("fill", "#74798C")
+    .text("Calories brûlées (kCal)");
+
+    //! axe X
+    svg
+      .append("g")
+      .attr("transform", `translate(0, ${height - 40})`)
+      .call(axisBottom(x));
+
+    //! axe Y droite (calories)
+    svg
+      .append("g")
+      .attr("transform", `translate(${width - 40}, 0)`)
+      .call(axisRight(yPoids));
+
+    //! label axe X
+    svg
+      .append("text")
+      .attr("x", width / 2)
+      .attr("y", height - 5)
+      .attr("text-anchor", "middle")
+
+    //! barres poids
     svg
       .selectAll(".poids")
       .data(sessions)
@@ -52,7 +116,7 @@ function Activite({ data }) {
       .attr("fill", "#282D30")
       .attr("rx", 3);
 
-    // barres calories (rouge)
+    //! barres calories
     svg
       .selectAll(".calories")
       .data(sessions)
@@ -68,6 +132,7 @@ function Activite({ data }) {
 
   }, [data]);
 
+  //! fonction min poids
   function minPoids(sessions) {
     return Math.min(...sessions.map(d => d.kilogram));
   }
